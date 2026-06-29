22466ae2f4f7cdebf28d61236c21e9311b2049b1
David <loorenz.david@gmail.com>
2026-06-29 18:57:07 +0200

    corrected migration deploy 2

--- app/migrations/versions/9a8b7c6d5e4f_merge_remaining_heads_after_upholstery_and_role_changes.py
+++ app/migrations/versions/9a8b7c6d5e4f_merge_remaining_heads_after_upholstery_and_role_changes.py
@@ -1,7 +1,7 @@
 """merge remaining heads after upholstery and role changes
 
 Revision ID: 9a8b7c6d5e4f
-Revises: 26d4b7f0c3aa, 3c2d4e5f6a7b, 71df9b8c4a2e, 7e1c3b4a9d2f
+Revises: 26d4b7f0c3aa, 3c2d4e5f6a7b, 7e1c3b4a9d2f
 Create Date: 2026-06-29 16:25:00.000000
 """
 
@@ -12,7 +12,6 @@ revision: str = "9a8b7c6d5e4f"
 down_revision: Union[str, Sequence[str], None] = (
     "26d4b7f0c3aa",
     "3c2d4e5f6a7b",
-    "71df9b8c4a2e",
     "7e1c3b4a9d2f",
 )
 branch_labels: Union[str, Sequence[str], None] = None
