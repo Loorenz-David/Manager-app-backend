b8c878d0ee2ad8c909bcce48de348280799e769c
David <loorenz.david@gmail.com>
2026-06-29 18:54:31 +0200

    fix migration deply

---
+++ app/migrations/versions/9a8b7c6d5e4f_merge_remaining_heads_after_upholstery_and_role_changes.py
@@ -0,0 +1,27 @@
+"""merge remaining heads after upholstery and role changes
+
+Revision ID: 9a8b7c6d5e4f
+Revises: 26d4b7f0c3aa, 3c2d4e5f6a7b, 71df9b8c4a2e, 7e1c3b4a9d2f
+Create Date: 2026-06-29 16:25:00.000000
+"""
+
+from typing import Sequence, Union
+
+
+revision: str = "9a8b7c6d5e4f"
+down_revision: Union[str, Sequence[str], None] = (
+    "26d4b7f0c3aa",
+    "3c2d4e5f6a7b",
+    "71df9b8c4a2e",
+    "7e1c3b4a9d2f",
+)
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
